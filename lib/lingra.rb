ac032ca9b7324b646bc70bb96218dcb1c72b0752
supermomonga <hi@supermomonga.com>
2015-02-19 22:08:52 +0900

    listup request methods

--- lib/lingra.rb
+++ lib/lingra.rb
@@ -24,6 +24,18 @@ module Lingra
 
     end
 
+    private
+    def request method, path, params = {}
+    end
+
+    def get path, params = {}
+      request :get, path, params
+    end
+
+    def post path, params = {}
+      request :post, path, params
+    end
+
   end
 
   class Session
